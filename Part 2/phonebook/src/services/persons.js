7dccb32d8949c3dad56f79f475780c9746592cba
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-18 18:46:29 +0530

    phonebook_backend_3.9

--- Part 2/phonebook/src/services/persons.js
+++ Part 2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -20,7 +20,7 @@ const update = (newobject) => {
   const request = axios.put(baseUrl+`/`+newobject.id,newobject)
   return request
   .then(response => response.data)
-  
+
 }
 export default {
   getAll: getAll,
