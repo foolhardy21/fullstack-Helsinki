593957c8084c479fa4c7693c5c170eaa030a2cf7
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-04-18 21:02:09 +0530

    phonebook-backend3.11

--- Part 2/phonebook/src/services/persons.js
+++ Part 2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://stark-sea-80466.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
