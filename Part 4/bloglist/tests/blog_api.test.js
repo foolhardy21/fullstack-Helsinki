50fab83c35ca9e342407ffefbf82a6427ac2a257
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-09-29 17:08:33 +0530

    bloglist backendtest 4.10

--- Part 4/bloglist/tests/blog_api.test.js
+++ Part 4/bloglist/tests/blog_api.test.js
@@ -6,20 +6,16 @@ const Blog = require('../models/Blog')
 const api = supertest(app)
 const initialBlogs = [
     {
-      _id: "5a422a851b54a676234d17f7",
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
       likes: 7,
-      __v: 0
     },
     {
-      _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
       likes: 5,
-      __v: 0
     },
 ]
 
@@ -52,7 +48,27 @@ describe('blog api', () => {
     test('unique identifier _id is defined', async () => {
         const response = await api.get('/api/blogs')
         expect(response.body[0]._id).toBeDefined()
-    })
+    }, 6000)
+
+    test('a new blog is added', async () => {
+        const newBlog = {
+            title: "Sample blog",
+            author: "Sample author",
+            url: "http://www.samplelink.com",
+            likes: 55,
+          }
+
+        await api.post('/api/blogs')
+                .send(newBlog)
+                .expect(201)
+
+        const response = await api.get('/api/blogs')
+        const titles = response.body.map(blog => blog.title)
+
+        expect(titles).toHaveLength(initialBlogs.length+1)
+        expect(titles).toContain('Sample blog')
+        
+    }, 10000)
 })
 
 afterAll(() => {
