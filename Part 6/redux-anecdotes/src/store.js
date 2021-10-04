e3c2323f523a85b173b69d3a9134b85bc8b4dc9b
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-10-04 16:24:24 +0530

    anecodtes with redux 6.9

---
+++ Part 6/redux-anecdotes/src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(reducer, composeWithDevTools())
+
+export default store
