2dc6b0e86ef0d6d3ccc1fd6a6171ccef44ea5043
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-10-11 20:05:57 +0530

    bloglist frontend 5.2

--- Part 5/bloglist-frontend/src/App.js
+++ Part 5/bloglist-frontend/src/App.js
@@ -14,30 +14,50 @@ const App = () => {
     blogsResponse = blogsResponse.filter(blog => blog.user.username === user.username)
     setBlogs(blogsResponse)
   }
+
+  function logOut() {
+    window.localStorage.removeItem('user')
+    setUser('')
+    setBlogs('')
+  }
+  
   function showNotification(text, duration) {
     setErrorMessage(text)
       setTimeout(() => {
         setErrorMessage('')
       },duration)
   }
+  
   async function handleSubmit(e) {
     e.preventDefault()
     const loginResponse = await getLoginToken(username, password)
     if(loginResponse.error) {
       showNotification(loginResponse.error, 3000)
     } else if(loginResponse.token) {
+      window.localStorage.setItem('user', JSON.stringify(loginResponse))
       setUser(loginResponse)
     }
+    setUsername('')
+    setPassword('')
   }
+  
+  useEffect(() => {
+    const savedUser = window.localStorage.getItem('user')
+    if(savedUser) {
+      setUser(JSON.parse(savedUser))
+    }
+  }, [])
   useEffect(() => {
     getAllBlogs()
-  }, [user])
+  },[user])
+  
   
   if(user) {
     return (
       <div>
         <h2>blogs</h2>
         <p>{user.username} logged in</p>
+        <button onClick={logOut}>logout</button>
         {
           blogs.map(blog => <Blog key={blog._id} blog={blog} />)
         }
@@ -50,9 +70,9 @@ const App = () => {
         <p>{errorMessage}</p>
         <form onSubmit={handleSubmit}>
           <label htmlFor='username_input'>username</label>
-          <input type='text' name='username_input' onChange={(e) => setUsername(e.target.value)}/><br/>
+          <input type='text' name='username_input' value={username} onChange={(e) => setUsername(e.target.value)}/><br/>
           <label htmlFor='password_input'>password</label>
-          <input type='password' name='password_input' onChange={(e) => setPassword(e.target.value)}/><br/>
+          <input type='password' name='password_input' value={password} onChange={(e) => setPassword(e.target.value)}/><br/>
           <input type='submit' value='login'/>
         </form>
       </div>
