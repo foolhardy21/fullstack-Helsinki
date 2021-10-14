f09d2641bfb7de8a10963ecb24c2966b96d4d13a
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-10-14 17:58:40 +0530

    bloglist react test 5.14

--- Part 5/bloglist-frontend/src/components/Blog/Blog.test.js
+++ Part 5/bloglist-frontend/src/components/Blog/Blog.test.js
@@ -1,24 +1,47 @@
 import React from "react"
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from "./Blog"
 
-test('blog by default should render title and author only', () => {
-    const blog = {
-        title: 'test title',
-        author: 'test author',
-        url: 'test url',
-        likes: 100
-    }
-    const getAllBlogs = jest.fn()
-    const token = 'testtoken'
+describe('Blog component rendering', () => {
 
-    const component = render(
-        <Blog blog={blog} token={token} getAllBlogs={getAllBlogs} />
-    )
+    test('blog by default should render title and author only', () => {
+        const blog = {
+            title: 'test title',
+            author: 'test author',
+            url: 'test url',
+            likes: 100
+        }
+        const getAllBlogs = jest.fn()
+        const token = 'testtoken'
+    
+        const component = render(
+            <Blog blog={blog} token={token} getAllBlogs={getAllBlogs} />
+        )
+        
+        expect(component.container).toHaveTextContent(`${blog.title}`)
+        expect(component.container).toHaveTextContent(`${blog.author}`)
+        expect(component.container.querySelector('#infodiv')).not.toBeVisible()
+    
+    })
 
-    expect(component.container).toHaveTextContent(`${blog.title}`)
-    expect(component.container).toHaveTextContent(`${blog.author}`)
-    expect(component.container.querySelector('#infodiv')).not.toBeVisible()
+    test('render url and likes on clicking more', () => {
+        const getAllBlogs = jest.fn()
+        const blog = {
+            title: 'test title',
+            author: 'test author',
+            url: 'test url',
+            likes: 100
+        }
+        const token='testtoken'
+
+        const component = render(
+            <Blog blog={blog} getAllBlogs={getAllBlogs} token={token} />
+        )
+        const button = component.getByText('more')
+        fireEvent.click(button)
+
+        expect(component.container.querySelector('#infodiv')).toBeVisible()
+    })
 
 })
